72c0a5a340e4ad1203b21f2fa0aaaf77ab5fbba0
Mustafa_Barakat <mustafabarakat633@gmail.com>
2025-11-21 12:59:13 +0200

    Services section is added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import AboutUs from "./sections/AboutUs";
 import HeroSection from "./sections/HeroSection";
+import Services from "./sections/Services";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <AboutUs />
+      <Services />
     </>
   );
 }
---
+++ app/sections/Services.tsx
@@ -0,0 +1,12 @@
+export default function Services() {
+  return (
+    <section className="bg-[url(/images/cars/car-2.png)] min-h-[95vh] bg-cover flex items-end py-24">
+      <div className="container">
+        <h2 className="uppercase text-white font-bold text-2xl sm:text-4xl md:text-6xl">
+          we are providing professional{" "}
+          <span className="text-primary">car services</span>
+        </h2>
+      </div>
+    </section>
+  );
+}

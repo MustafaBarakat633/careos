43a702574302501ec9c7b5e25dfee3c443dd4f49
Mustafa_Barakat <mustafabarakat633@gmail.com>
2025-11-22 20:06:13 +0200

    Use Swiper instead of React Slick

--- components/layout/CarsSlider.tsx
+++ components/layout/CarsSlider.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { useRef } from "react";
-import Slider from "react-slick";
-
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
 
 import IconButton from "../ui/IconButton";
 import { IoMdArrowRoundBack, IoMdArrowRoundForward } from "react-icons/io";
@@ -20,95 +19,69 @@ type CarSliderProps = {
 };
 
 export default function CarsSlider({ cars }: CarSliderProps) {
-  const sliderRef = useRef<Slider | null>(null);
-
-  const next = () => {
-    sliderRef.current?.slickNext();
-  };
-
-  const previous = () => {
-    sliderRef.current?.slickPrev();
-  };
-
-  const settings = {
-    dots: false,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: false,
-    speed: 500,
-    autoplaySpeed: 2000,
-    cssEase: "linear",
-    // initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          // initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 550,
-        settings: {
-          slidesToShow: 1,
-          // slidesToScroll: 1,
-          dots: false,
-        },
-      },
-    ],
-  };
-
   return (
-    <div className="slider-container">
+    <div className="relative">
       <div className="flex justify-end gap-3 mb-5">
-        <IconButton
-          icon={<IoMdArrowRoundBack size={20} />}
-          onClick={previous}
-        />
-        <IconButton icon={<IoMdArrowRoundForward size={20} />} onClick={next} />
+        <IconButton id="previous" icon={<IoMdArrowRoundBack size={20} />} />
+        <IconButton id="next" icon={<IoMdArrowRoundForward size={20} />} />
       </div>
 
-      <Slider ref={sliderRef} {...settings}>
+      <Swiper
+        modules={[Navigation, Autoplay]}
+        navigation={{
+          prevEl: "#previous",
+          nextEl: "#next",
+        }}
+        slidesPerView={1}
+        spaceBetween={20}
+        loop={true}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+          },
+
+          768: {
+            slidesPerView: 2.5,
+          },
+
+          1024: {
+            slidesPerView: 3,
+          },
+
+          1280: {
+            slidesPerView: 3.5,
+          },
+        }}
+      >
         {cars.map((car, i) => (
-          <div
-            key={i}
-            className="bg-white min-h-[400px] relative group overflow-hidden"
-          >
-            <div className="absolute inset-0 z-1">
-              <Image
-                src={car.image}
-                fill
-                alt="car"
-                className="object-cover group-hover:scale-105 duration-200"
-              />
-            </div>
-
-            <div className="relative z-2 text-white backdrop-blur-lg bg-black/30 p-4">
-              <h3 className="font-bold mb-1">{car.title}</h3>
-              <p className="text-xs">{car.description}</p>
-            </div>
-
-            {car.badge && (
-              <div className="absolute z-2 px-6 py-4 bottom-0 flex w-full gap-2 items-center">
-                <p className="uppercase text-primary text-xl font-bold text-nowrap">
-                  hot sell
-                </p>
-                <span className="h-0.5 bg-gray-300 w-full"></span>
+          <SwiperSlide key={i}>
+            <div className="bg-white min-h-[400px] relative group overflow-hidden">
+              <div className="absolute inset-0 z-1 w-full">
+                <Image
+                  src={car.image}
+                  fill
+                  alt="car"
+                  className="object-cover bg-center group-hover:scale-105 duration-200"
+                />
               </div>
-            )}
-          </div>
+
+              <div className="relative z-2 text-white backdrop-blur-lg bg-black/30 p-4">
+                <h3 className="font-bold mb-1">{car.title}</h3>
+                <p className="text-xs">{car.description}</p>
+              </div>
+
+              {car.badge && (
+                <div className="absolute z-2 px-6 py-4 bottom-0 flex w-full gap-2 items-center">
+                  <p className="uppercase text-primary text-xl font-bold text-nowrap">
+                    hot sell
+                  </p>
+                  <span className="h-0.5 bg-gray-300 w-full"></span>
+                </div>
+              )}
+            </div>
+          </SwiperSlide>
         ))}
-      </Slider>
+      </Swiper>
     </div>
   );
 }
